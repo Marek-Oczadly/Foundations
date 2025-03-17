d9d6b89dcb2e77587410e0dee2d17413a0155d89
Marek Oczadly <moczadly@outlook.com>
2025-03-17 17:32:02 +0000

    Made the Matrix implementation templated

--- Foundations/Foundations.cpp
+++ Foundations/Foundations.cpp
@@ -5,9 +5,12 @@
 #include "euclids_algorithm.h"
 #include "LinearAlgebra.h"
 
-int main()
-{
-	Matrix m(3, 3);
+int main() {
+	Matrix<int> m(3, 3);
+	Vector<int> v = { 1, 2, 3 };
+	IdentityMatrix<int> i(3);
 	std::cout << m.display() << std::endl;
+	std::cout << v.display() << std::endl;
+	std::cout << i.display() << std::endl;
 	return 0;
 }
--- Foundations/LinearAlgebra.h
+++ Foundations/LinearAlgebra.h
@@ -5,18 +5,20 @@
 #include <iostream>
 #include <stdexcept>
 
+
+template <typename T>
 class Matrix {
-private:
+protected:
 	const unsigned int ROWS;
 	const unsigned int COLS;
 
-	double** const DATA;
+	T** const DATA;
 
-	double** const allocate(const unsigned int rows, const unsigned int cols) const {
-		double** data = new double* [rows];
-		for (double** row = data; row < data + rows; ++row) {
-			*row = new double[cols];
-			for (double* val = *row; val < *row + cols; ++val) {
+	T** const allocate(const unsigned int rows, const unsigned int cols) const {
+		T** data = new T* [rows];
+		for (T** row = data; row < data + rows; ++row) {
+			*row = new T[cols];
+			for (T* val = *row; val < *row + cols; ++val) {
 				*val = 0;
 			}
 		}
@@ -31,7 +33,7 @@ public:
 	}
 
 	~Matrix() {
-		for (double** row = DATA; row < DATA + ROWS; ++row) {
+		for (T** row = DATA; row < DATA + ROWS; ++row) {
 			delete[] * row;
 		}
 		delete[] DATA;
@@ -39,9 +41,9 @@ public:
 	
 	std::string display() const noexcept {
 		std::string str = "";
-		for (const double* const * row = DATA; row < DATA + ROWS; ++row) {
+		for (const T* const * row = DATA; row < DATA + ROWS; ++row) {
 			str += "[";
-			for (const double* val = *row; val < *row + COLS - 1; ++val) {
+			for (const T* val = *row; val < *row + COLS - 1; ++val) {
 				str += std::to_string(*val) + "  ";
 			}
 			str += std::to_string(*(*row + COLS - 1)) + "]\n";
@@ -49,9 +51,42 @@ public:
 		return str;
 	}
 	
-	 double* const operator[](const unsigned int row) {
+	 T* const operator[](const unsigned int row) {
 		 if (row > ROWS) throw std::out_of_range("The row does not correspond to a row in the matrix");
 		 return *(DATA + row);
 	 }
 
+};
+
+
+template <typename T>
+class Vector : public Matrix<T> {
+public:
+	Vector(const unsigned int size) : Matrix<T>(size, 1) {}
+	Vector(std::initializer_list<T> list) : Matrix<T>(list.size(), 1) {
+		unsigned int i = 0;
+		for (T val : list) {
+			*(*(this->DATA + i++)) = val;
+		}
+	}
+
+	std::string display() const noexcept {
+		std::string str = "";
+		for (const T* const* row = this->DATA; row < this->DATA + this->ROWS - 1; ++row) {
+			str += "[" + std::to_string(**row) + "]\n";
+		}
+		str += "[" + std::to_string(**(this->DATA + this->ROWS - 1)) + "]";
+		return str;
+	}
+};
+
+
+template <typename T>
+class IdentityMatrix : public Matrix<T> {
+public:
+	IdentityMatrix(const unsigned int size) : Matrix<T>(size, size) {
+		for (unsigned int i = 0; i < size; ++i) {
+			*(*(this->DATA + i) + i) = 1;
+		}
+	}
 };
